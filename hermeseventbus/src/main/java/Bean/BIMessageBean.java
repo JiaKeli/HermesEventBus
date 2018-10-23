8d6c304c07f56d32d441be3d638b2987aad72033
jiakeli <jiakeli@cibnvideo.com>
2018-10-23 15:54:38 +0800

    修改HermesEventBus适应27

---
+++ hermeseventbus/src/main/java/Bean/BIMessageBean.java
@@ -0,0 +1,44 @@
+package Bean;
+
+/**
+ * Created by Corey_Jia on 2018/10/23.
+ */
+public class BIMessageBean {
+    public static String TAG_REPORT;
+    public static int WHAT_BI_COMMON = 1001;
+    public static int WHAT_BI_AD = 1002;
+
+    private String tag;
+    private int what;
+    private String json;
+
+    public BIMessageBean(String tag, int what, String json) {
+        this.tag = tag;
+        this.what = what;
+        this.json = json;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getWhat() {
+        return what;
+    }
+
+    public void setWhat(int what) {
+        this.what = what;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+}
